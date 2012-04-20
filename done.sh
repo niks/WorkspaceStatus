f4e8d886e901c1db1b44f22765637f66c2fd277e
Michael Schnell <Michael-Schnell@gmx.de>
2012-04-20 07:46:47 +0200

    Add done.sh, a script that prints the current work of the user
    The script searches for git repositories and prints out all
    commits that the user added in a configurable short time slot
    like "today" or "yesterday".

---
+++ done.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+before=now
+
+if test "$1" = "after" 
+then
+	shift
+	after="$*"
+elif test "$*" = "today" 
+then
+	after="00:00"
+elif test "$*" = "yesterday" 
+then
+	after="yesterday 00:00"
+	before="00:00"
+elif test "$*" = "this week"
+then 
+	after="one week ago"
+elif test "$*" = "this month"
+then 
+	after="one month ago"
+else 
+	echo "Don't understand $*. Assuming today"
+	after="00:00"
+fi
+
+echo log commits done after $after and before $before
+
+find . -name ".git" | 
+while read dir
+do 
+	user=`git config user.name`
+	echo done as $user in ${dir%.git}
+	git --git-dir=$dir log --oneline --after="$after" --before="$before" --author="$user" --all
+done
